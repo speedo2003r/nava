cd70bab38e500f2c3d66ca44a962697cf4c449d3
khaled reda <speedo2003r@gmail.com>
2022-04-30 14:42:40 +0000

    change service categories in tech

--- app/Http/Controllers/Api/Tech/CategoryController.php
+++ app/Http/Controllers/Api/Tech/CategoryController.php
@@ -35,6 +35,7 @@ class CategoryController extends Controller{
         $order = $this->orderRepo->find($request['order_id']);
         $category = $this->categoryRepo->find($order['category_id']);
         $subCategories = $category->children;
+        Cache::forget('order_service_'.$order['id'].'_'.$order->user['id']);
         return $this->successResponse(CategoryResource::collection($subCategories));
     }
 
@@ -42,26 +43,18 @@ class CategoryController extends Controller{
     {
         $validator = Validator::make($request->all(),[
             'category_id' => 'required|exists:categories,id,deleted_at,NULL',
-            'order_id' => 'sometimes|exists:orders,id,deleted_at,NULL',
         ]);
         if($validator->fails()){
             return $this->ApiResponse('fail',$validator->errors()->first());
         }
         $data = [];
         $services = $this->service->where('category_id',$request['category_id'])->get();
-        $servicesData = null;
-        if(isset($request['order_id'])){
-            $order = $this->orderRepo->find($request['order_id']);
-            $servicesData = Cache::get('order_service_'.$order['id'].'_'.$order->user['id']);
-        }
         foreach ($services as $service){
             $data[] = [
                 'id' => $service['id'],
                 'title' => $service['title'],
                 'description' => $service['description'],
                 'price' => $service['price'],
-                'checked' => $servicesData && $servicesData->where('id',$service['id'])->first() ? true : false,
-                'count' => $servicesData && $servicesData->where('id',$service['id'])->first() ? ((int) $servicesData->where('id',$service['id'])->first()['count']) : 0,
             ];
         }
         return $this->successResponse($data);
